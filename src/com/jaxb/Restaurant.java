2c0118fdb03c798c3bc2005abfa561b9948e1161
cjl <605385881@qq.com>
2019-05-22 08:52:51 +0800

    复杂信息

---
+++ src/com/jaxb/Restaurant.java
@@ -0,0 +1,30 @@
+package com.jaxb;
+
+public class Restaurant {
+    private String restaurantName;
+    private String restaurantAddress;
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getRestaurantAddress() {
+        return restaurantAddress;
+    }
+
+    public void setRestaurantAddress(String restaurantAddress) {
+        this.restaurantAddress = restaurantAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "restaurantName='" + restaurantName + '\'' +
+                ", restaurantAddress='" + restaurantAddress + '\'' +
+                '}';
+    }
+}
